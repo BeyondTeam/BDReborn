2f5887c6ddcc7417ebc0568a19850a4d93aaabeb
Makan <makan.j5050@gmail.com>
2017-01-17 17:38:26 +0330

    Update beyond.sh

--- beyond.sh
+++ beyond.sh
@@ -6,14 +6,20 @@ install() {
 	    cd tg
 		sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 		sudo apt-get install g++-4.7 c++-4.7
-		sudo apt-get upgrade
+		sudo apt-get update		
+                sudo apt-get upgrade
 		sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
+		sudo apt-get install lua-lgi
+		sudo apt-get install libnotify-dev
 		sudo apt-get install screen
 		sudo apt-get install tmux
 		wget https://valtman.name/files/telegram-cli-1222
 		mv telegram-cli-1222 tgcli
 		chmod +x tgcli
 		cd ..
+		chmod +x bot
+		chmod +x beyond.sh
+		chmod +x tg
 }
 
 if [ "$1" = "install" ]; then

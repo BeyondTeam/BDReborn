ce02febb38730f2a15addcf9c00c60c16416714c
ali <ali-ghoghnoos@users.noreply.github.com>
2017-01-27 22:39:31 +0330

    now , all TG-CLI arguments support!
    Now you can launch beyond(Telegram-cli) with that objects!
    EX :
    ./beyond.sh --json
    For running in json mode !

--- beyond.sh
+++ beyond.sh
@@ -7,7 +7,7 @@ install() {
 		sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 		sudo apt-get install g++-4.7 c++-4.7
 		sudo apt-get update		
-                sudo apt-get upgrade
+        sudo apt-get upgrade
 		sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
 		sudo apt-get install lua-lgi
 		sudo apt-get install libnotify-dev
@@ -30,17 +30,45 @@ if [ ! -f ./tg/tgcli ]; then
     echo "Run $0 install"
     exit 1
 fi
-
-if [ "$1" = "-p" ]; then
-    echo -e "\033[38;5;208m"
-    echo -e "     > BeyondTeam Source :D                        "
-    echo -e "                                              \033[0;00m"
-    echo -e "\e[36m"
-    ./tg/tgcli -s ./bot/bot.lua -p $2
-fi
    echo -e "\033[38;5;208m"
    echo -e "     > BeyondTeam Source :D                        "
    echo -e "                                              \033[0;00m"
    echo -e "\e[36m"
-   ./tg/tgcli -s ./bot/bot.lua -l 1 -E $@
+   ./tg/tgcli -s ./bot/bot.lua $@
 fi
+
+# Now All Argument Support after ./beyond.sh !
+#	Arguments :
+#			#			#			#			#			#			#			#			#			#
+#  --phone/-u                           specify username (would not be asked during authorization)
+#  --verbosity/-v                       increase verbosity (0-ERROR 1-WARNIN 2-NOTICE 3+-DEBUG-levels)
+#  --enable-msg-id/-N                   message num mode
+#  --config/-c                          config file name
+#  --profile/-p                         use specified profile
+#  --wait-dialog-list/-W                send dialog_list query and wait for answer before reading input
+#  --disable-colors/-C                  disable color output
+#  --disable-readline/-R                disable readline
+#  --alert/-A                           enable bell notifications
+#  --daemonize/-d                       daemon mode
+#  --logname/-L <log-name>              log file name
+#  --username/-U <user-name>            change uid after start
+#  --groupname/-G <group-name>          change gid after start
+#  --disable-output/-D                  disable output
+#  --tcp-port/-P <port>                 port to listen for input commands
+#  --udp-socket/-S <socket-name>        unix socket to create
+#  --exec/-e <commands>                 make commands end exit
+#  --disable-names/-I                   use user and chat IDs in updates instead of names
+#  --help/-h                            prints this help
+#  --accept-any-tcp                     accepts tcp connections from any src (only loopback by default)
+#  --disable-link-preview               disables server-side previews to links
+#  --bot/-b                             bot mode
+#  --json                               prints answers and values in json format
+#  --permanent-msg-ids                  use permanent msg ids
+#  --permanent-peer-ids                 use permanent peer ids
+#			#			#			#			#			#			#			#			#			#
+#Example To launch with second profile :
+# ./beyond.sh -p second-profile
+
+#			  OR
+
+# ./beyond.sh --profile second-profile

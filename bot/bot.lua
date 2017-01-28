c9c03b0dc3cb8f28652529642a37c19e63995d1c
SoLiD <solid.in89@yahoo.com>
2017-01-28 23:14:40 +0330

    Bot updated to V1.7

--- bot/bot.lua
+++ bot/bot.lua
@@ -88,7 +88,7 @@ function create_config( )
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》Beyond Reborn v1
+    info_text = [[》Beyond Reborn v1.7
 An advanced administration bot based on https://valtman.name/telegram-cli
 
 》https://github.com/BeyondTeam/BDReborn 

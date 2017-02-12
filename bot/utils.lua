010dc43cb3d5f54eb09d45f9ec3440737e5c3ed5
ToOfan <toofaneariyayi@gmail.com>
2017-02-12 22:34:07 +0330

    BDReborn v3.0

---
+++ bot/utils.lua
@@ -0,0 +1,644 @@
+--Begin Utils.lua By #BeyondTeam :)
+function serialize_to_file(data, file, uglify)
+  file = io.open(file, 'w+')
+  local serialized
+  if not uglify then
+    serialized = serpent.block(data, {
+        comment = false,
+        name = '_'
+      })
+  else
+    serialized = serpent.dump(data)
+  end
+  file:write(serialized)
+  file:close()
+end
+function string.random(length)
+   local str = "";
+   for i = 1, length do
+      math.random(97, 122)
+      str = str..string.char(math.random(97, 122));
+   end
+   return str;
+end
+
+function string:split(sep)
+  local sep, fields = sep or ":", {}
+  local pattern = string.format("([^%s]+)", sep)
+  self:gsub(pattern, function(c) fields[#fields+1] = c end)
+  return fields
+end
+
+-- DEPRECATED
+function string.trim(s)
+  print("string.trim(s) is DEPRECATED use string:trim() instead")
+  return s:gsub("^%s*(.-)%s*$", "%1")
+end
+
+-- Removes spaces
+function string:trim()
+  return self:gsub("^%s*(.-)%s*$", "%1")
+end
+
+function get_http_file_name(url, headers)
+  -- Eg: foo.var
+  local file_name = url:match("[^%w]+([%.%w]+)$")
+  -- Any delimited alphanumeric on the url
+  file_name = file_name or url:match("[^%w]+(%w+)[^%w]+$")
+  -- Random name, hope content-type works
+  file_name = file_name or str:random(5)
+
+  local content_type = headers["content-type"]
+
+  local extension = nil
+  if content_type then
+    extension = mimetype.get_mime_extension(content_type)
+  end
+  if extension then
+    file_name = file_name.."."..extension
+  end
+
+  local disposition = headers["content-disposition"]
+  if disposition then
+    -- attachment; filename=CodeCogsEqn.png
+    file_name = disposition:match('filename=([^;]+)') or file_name
+  end
+
+  return file_name
+end
+
+--  Saves file to /tmp/. If file_name isn't provided,
+-- will get the text after the last "/" for filename
+-- and content-type for extension
+function download_to_file(url, file_name)
+  print("url to download: "..url)
+
+  local respbody = {}
+  local options = {
+    url = url,
+    sink = ltn12.sink.table(respbody),
+    redirect = true
+  }
+
+  -- nil, code, headers, status
+  local response = nil
+
+  if url:starts('https') then
+    options.redirect = false
+    response = {https.request(options)}
+  else
+    response = {http.request(options)}
+  end
+
+  local code = response[2]
+  local headers = response[3]
+  local status = response[4]
+
+  if code ~= 200 then return nil end
+
+  file_name = file_name or get_http_file_name(url, headers)
+
+  local file_path = "data/tmp/"..file_name
+  print("Saved to: "..file_path)
+
+  file = io.open(file_path, "w+")
+  file:write(table.concat(respbody))
+  file:close()
+
+  return file_path
+end
+function run_command(str)
+  local cmd = io.popen(str)
+  local result = cmd:read('*all')
+  cmd:close()
+  return result
+end
+function string:isempty()
+  return self == nil or self == ''
+end
+
+-- Returns true if the string is blank
+function string:isblank()
+  self = self:trim()
+  return self:isempty()
+end
+
+-- DEPRECATED!!!!!
+function string.starts(String, Start)
+  print("string.starts(String, Start) is DEPRECATED use string:starts(text) instead")
+  return Start == string.sub(String,1,string.len(Start))
+end
+
+-- Returns true if String starts with Start
+function string:starts(text)
+  return text == string.sub(self,1,string.len(text))
+end
+function unescape_html(str)
+  local map = {
+    ["lt"]  = "<",
+    ["gt"]  = ">",
+    ["amp"] = "&",
+    ["quot"] = '"',
+    ["apos"] = "'"
+  }
+  new = string.gsub(str, '(&(#?x?)([%d%a]+);)', function(orig, n, s)
+    var = map[s] or n == "#" and string.char(s)
+    var = var or n == "#x" and string.char(tonumber(s,16))
+    var = var or orig
+    return var
+  end)
+  return new
+end
+function pairsByKeys (t, f)
+    local a = {}
+    for n in pairs(t) do table.insert(a, n) end
+    table.sort(a, f)
+    local i = 0      -- iterator variable
+    local iter = function ()   -- iterator function
+      i = i + 1
+		if a[i] == nil then return nil
+		else return a[i], t[a[i]]
+		end
+	end
+	return iter
+end
+
+function scandir(directory)
+  local i, t, popen = 0, {}, io.popen
+  for filename in popen('ls -a "'..directory..'"'):lines() do
+    i = i + 1
+    t[i] = filename
+  end
+  return t
+end
+
+function plugins_names( )
+  local files = {}
+  for k, v in pairs(scandir("plugins")) do
+    -- Ends with .lua
+    if (v:match(".lua$")) then
+      table.insert(files, v)
+    end
+  end
+  return files
+end
+
+-- Function name explains what it does.
+function file_exists(name)
+  local f = io.open(name,"r")
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
+end
+
+function gp_type(chat_id)
+  local gp_type = "pv"
+  local id = tostring(chat_id)
+    if id:match("^-100") then
+      gp_type = "channel"
+    elseif id:match("-") then
+      gp_type = "chat"
+  end
+  return gp_type
+end
+
+function is_reply(msg)
+  local var = false
+    if msg.reply_to_message_id_ ~= 0 then -- reply message id is not 0
+      var = true
+    end
+  return var
+end
+
+function is_supergroup(msg)
+  chat_id = tostring(msg.to.id)
+  if chat_id:match('^-100') then --supergroups and channels start with -100
+    if not msg.is_post_ then
+    return true
+    end
+  else
+    return false
+  end
+end
+
+function is_channel(msg)
+  chat_id = tostring(msg.to.id)
+  if chat_id:match('^-100') then -- Start with -100 (like channels and supergroups)
+  if msg.is_post_ then -- message is a channel post
+    return true
+  else
+    return false
+  end
+  end
+end
+
+function is_group(msg)
+  chat_id = tostring(msg.to.id)
+  if chat_id:match('^-100') then --not start with -100 (normal groups does not have -100 in first)
+    return false
+  elseif chat_id:match('^-') then
+    return true
+  else
+    return false
+  end
+end
+
+function is_private(msg)
+  chat_id = tostring(msg.to.id)
+  if chat_id:match('^-') then --private chat does not start with -
+    return false
+  else
+    return true
+  end
+end
+
+function check_markdown(text) --markdown escape ( when you need to escape markdown , use it like : check_markdown('your text')
+		str = text
+		if str:match('_') then
+			output = str:gsub('_','\\_')
+		elseif str:match('*') then
+			output = str:gsub('*','\\*')
+		elseif str:match('`') then
+			output = str:gsub('`','\\`')
+		else
+			output = str
+		end
+	return output
+end
+
+function is_sudo(msg)
+  local var = false
+  -- Check users id in config
+  for v,user in pairs(_config.sudo_users) do
+    if user == msg.from.id then
+      var = true
+    end
+  end
+  return var
+end
+
+function is_owner(msg)
+  local var = false
+  local data = load_data(_config.moderation.data)
+  local user = msg.from.id
+  if data[tostring(msg.to.id)] then
+    if data[tostring(msg.to.id)]['owners'] then
+      if data[tostring(msg.to.id)]['owners'][tostring(user)] then
+        var = true
+      end
+    end
+  end
+
+  for v,user in pairs(_config.admins) do
+    if user[1] == msg.from.id then
+      var = true
+  end
+end
+
+  for v,user in pairs(_config.sudo_users) do
+    if user == msg.from.id then
+        var = true
+    end
+  end
+  return var
+end
+
+function is_admin(msg)
+  local var = false
+  local user = msg.from.id
+  for v,user in pairs(_config.admins) do
+    if user[1] == user then
+      var = true
+  end
+end
+
+  for v,user in pairs(_config.sudo_users) do
+    if user == user then
+        var = true
+    end
+  end
+  return var
+end
+
+--Check if user is the mod of that group or not
+function is_mod(msg)
+  local var = false
+  local data = load_data(_config.moderation.data)
+  local usert = msg.from.id
+  if data[tostring(msg.to.id)] then
+    if data[tostring(msg.to.id)]['mods'] then
+      if data[tostring(msg.to.id)]['mods'][tostring(usert)] then
+        var = true
+      end
+    end
+  end
+
+  if data[tostring(msg.to.id)] then
+    if data[tostring(msg.to.id)]['owners'] then
+      if data[tostring(msg.to.id)]['owners'][tostring(usert)] then
+        var = true
+      end
+    end
+  end
+
+  for v,user in pairs(_config.admins) do
+    if user[1] == msg.from.id then
+      var = true
+  end
+end
+
+  for v,user in pairs(_config.sudo_users) do
+    if user == msg.from.id then
+        var = true
+    end
+  end
+  return var
+end
+
+function is_sudo1(user_id)
+  local var = false
+  -- Check users id in config
+  for v,user in pairs(_config.sudo_users) do
+    if user == user_id then
+      var = true
+    end
+  end
+  return var
+end
+
+function is_owner1(chat_id, user_id)
+  local var = false
+  local data = load_data(_config.moderation.data)
+  local user = user_id
+  if data[tostring(chat_id)] then
+    if data[tostring(chat_id)]['owners'] then
+      if data[tostring(chat_id)]['owners'][tostring(user)] then
+        var = true
+      end
+    end
+  end
+
+  for v,user in pairs(_config.admins) do
+    if user[1] == user_id then
+      var = true
+  end
+end
+
+  for v,user in pairs(_config.sudo_users) do
+    if user == user_id then
+        var = true
+    end
+  end
+  return var
+end
+
+function is_admin1(user_id)
+  local var = false
+  local user = user_id
+  for v,user in pairs(_config.admins) do
+    if user[1] == user_id then
+      var = true
+  end
+end
+
+  for v,user in pairs(_config.sudo_users) do
+    if user == user_id then
+        var = true
+    end
+  end
+  return var
+end
+
+--Check if user is the mod of that group or not
+function is_mod1(chat_id, user_id)
+  local var = false
+  local data = load_data(_config.moderation.data)
+  local usert = user_id
+  if data[tostring(chat_id)] then
+    if data[tostring(chat_id)]['mods'] then
+      if data[tostring(chat_id)]['mods'][tostring(usert)] then
+        var = true
+      end
+    end
+  end
+
+  if data[tostring(chat_id)] then
+    if data[tostring(chat_id)]['owners'] then
+      if data[tostring(chat_id)]['owners'][tostring(usert)] then
+        var = true
+      end
+    end
+  end
+
+  for v,user in pairs(_config.admins) do
+    if user[1] == user_id then
+      var = true
+  end
+end
+
+  for v,user in pairs(_config.sudo_users) do
+    if user == user_id then
+        var = true
+    end
+  end
+  return var
+end
+
+ function is_banned(user_id, chat_id)
+  local var = false
+  local data = load_data(_config.moderation.data)
+  if data[tostring(chat_id)] then
+    if data[tostring(chat_id)]['banned'] then
+      if data[tostring(chat_id)]['banned'][tostring(user_id)] then
+        var = true
+      end
+    end
+  end
+return var
+end
+
+ function is_silent_user(user_id, chat_id)
+  local var = false
+  local data = load_data(_config.moderation.data)
+  if data[tostring(chat_id)] then
+    if data[tostring(chat_id)]['is_silent_users'] then
+      if data[tostring(chat_id)]['is_silent_users'][tostring(user_id)] then
+        var = true
+      end
+    end
+  end
+return var
+end
+
+function is_gbanned(user_id)
+  local var = false
+  local data = load_data(_config.moderation.data)
+  local user = user_id
+  local gban_users = 'gban_users'
+  if data[tostring(gban_users)] then
+    if data[tostring(gban_users)][tostring(user)] then
+      var = true
+    end
+  end
+return var
+end
+
+function is_filter(msg, text)
+local var = false
+local data = load_data(_config.moderation.data)
+  if data[tostring(msg.to.id)]['filterlist'] then
+for k,v in pairs(data[tostring(msg.to.id)]['filterlist']) do 
+    if string.find(string.lower(text), string.lower(k)) then
+       var = true
+        end
+     end
+  end
+ return var
+end
+
+function kick_user(user_id, chat_id)
+if not tonumber(user_id) then
+return false
+end
+  tdcli.changeChatMemberStatus(chat_id, user_id, 'Kicked', dl_cb, nil)
+end
+
+function del_msg(chat_id, message_ids)
+local msgid = {[0] = message_ids}
+  tdcli.deleteMessages(chat_id, msgid, dl_cb, nil)
+end
+
+ function banned_list(chat_id)
+local hash = "gp_lang:"..chat_id
+local lang = redis:get(hash)
+    local data = load_data(_config.moderation.data)
+    local i = 1
+  if not data[tostring(msg.chat_id_)] then
+  if not lang then
+    return '_Group is not added_'
+else
+    return 'گروه به لیست گروه های مدیریتی ربات اضافه نشده است'
+   end
+  end
+  -- determine if table is empty
+  if next(data[tostring(chat_id)]['banned']) == nil then --fix way
+     if not lang then
+					return "_No_ *banned* _users in this group_"
+   else
+					return "*هیچ کاربری از این گروه محروم نشده*"
+              end
+				end
+       if not lang then
+   message = '*List of banned users :*\n'
+         else
+   message = '_لیست کاربران محروم شده از گروه :_\n'
+     end
+  for k,v in pairs(data[tostring(chat_id)]['banned']) do
+    message = message ..i.. '- '..v..' [' ..k.. '] \n'
+   i = i + 1
+end
+  return message
+end
+
+ function silent_users_list(chat_id)
+local hash = "gp_lang:"..chat_id
+local lang = redis:get(hash)
+    local data = load_data(_config.moderation.data)
+    local i = 1
+  if not data[tostring(msg.chat_id_)] then
+  if not lang then
+    return '_Group is not added_'
+else
+    return 'گروه به لیست گروه های مدیریتی ربات اضافه نشده است'
+   end
+  end
+  -- determine if table is empty
+  if next(data[tostring(chat_id)]['is_silent_users']) == nil then --fix way
+        if not lang then
+					return "_No_ *silent* _users in this group_"
+   else
+					return "*لیست کاربران سایلنت شده خالی است*"
+             end
+				end
+      if not lang then
+   message = '*List of silent users :*\n'
+       else
+   message = '_لیست کاربران سایلنت شده :_\n'
+    end
+  for k,v in pairs(data[tostring(chat_id)]['is_silent_users']) do
+    message = message ..i.. '- '..v..' [' ..k.. '] \n'
+   i = i + 1
+end
+  return message
+end
+
+ function gbanned_list(msg)
+local hash = "gp_lang:"..msg.chat_id_
+local lang = redis:get(hash)
+    local data = load_data(_config.moderation.data)
+    local i = 1
+  if not data['gban_users'] then
+    data['gban_users'] = {}
+    save_data(_config.moderation.data, data)
+  end
+  if next(data['gban_users']) == nil then --fix way
+    if not lang then
+					return "_No_ *globally banned* _users available_"
+   else
+					return "*هیچ کاربری از گروه های ربات محروم نشده*"
+             end
+				end
+        if not lang then
+   message = '*List of globally banned users :*\n'
+   else
+   message = '_لیست کاربران محروم شده از گروه های ربات :_\n'
+   end
+  for k,v in pairs(data['gban_users']) do
+    message = message ..i.. '- '..v..' [' ..k.. '] \n'
+   i = i + 1
+end
+  return message
+end
+
+ function filter_list(msg)
+local hash = "gp_lang:"..msg.chat_id_
+local lang = redis:get(hash)
+    local data = load_data(_config.moderation.data)
+  if not data[tostring(msg.chat_id_)]['filterlist'] then
+    data[tostring(msg.chat_id_)]['filterlist'] = {}
+    save_data(_config.moderation.data, data)
+    end
+  if not data[tostring(msg.chat_id_)] then
+  if not lang then
+    return '_Group is not added_'
+else
+    return 'گروه به لیست گروه های مدیریتی ربات اضافه نشده است'
+   end
+  end
+  -- determine if table is empty
+  if next(data[tostring(msg.chat_id_)]['filterlist']) == nil then --fix way
+      if not lang then
+    return "*Filtered words list* _is empty_"
+      else
+    return "_لیست کلمات فیلتر شده خالی است_"
+     end
+  end
+  if not data[tostring(msg.chat_id_)]['filterlist'] then
+    data[tostring(msg.chat_id_)]['filterlist'] = {}
+    save_data(_config.moderation.data, data)
+    end
+      if not lang then
+       filterlist = '*List of filtered words :*\n'
+         else
+       filterlist = '_لیست کلمات فیلتر شده :_\n'
+    end
+ local i = 1
+   for k,v in pairs(data[tostring(msg.chat_id_)]['filterlist']) do
+              filterlist = filterlist..'*'..i..'* - _'..k..'_\n'
+             i = i + 1
+         end
+     return filterlist
+   end

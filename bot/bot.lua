54aecb97e68917989e16e7672acdd979decccadf
datak137 <mohamadrezaahmadi137@gmail.com>
2017-03-28 13:51:17 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -106,7 +106,7 @@ function create_config( )
 	cmd = '^[/!#]',
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》Beyond Reborn v4.0
+    info_text = [[》Beyond Reborn v4.4
 An advanced administration bot based on https://valtman.name/telegram-cli
 
 》https://github.com/BeyondTeam/BDReborn 

21771b4d656730f9799321111efb143911bd1b9d
ToOfan <toofaneariyayi@gmail.com>
2017-05-25 05:48:42 +0430

    BDReborn V6 (Final Version)

--- bot/bot.lua
+++ bot/bot.lua
@@ -19,6 +19,7 @@ local notify = lgi.require('Notify')
 notify.init ("Telegram updates")
 chats = {}
 plugins = {}
+helper_id = 340155871 --Put Your Helper Bot ID Here
 
 function do_notify (user, msg)
 	local n = notify.Notification.new(user, msg)
